1da48ad466b0cf8ea59ca4e4ba4e8e218a013a2e
Allyson Goff <allysongoff@gmail.com>
2023-05-26 16:18:09 -0400

    added c++ file

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: aegoff
+ *        Date: 5/26/2023
+ * Description: my description
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const* argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
